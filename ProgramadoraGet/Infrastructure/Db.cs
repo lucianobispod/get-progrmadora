d85d061d919d223d9be3569dd3304beae4b439a6
lucianobispom <lucianobispom@gmail.com>
2018-03-01 22:07:50 -0300

    Classe Db criada

---
+++ ProgramadoraGet/Infrastructure/Db.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramadoraGet.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Infrastructure
+{
+    public class Db : DbContext
+    {
+        public Db(DbContextOptions<Db> options) : base(options) { }
+
+        #region Tables
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder m)
+        {
+            base.OnModelCreating(m);
+                       
+        }
+
+    }
+}
