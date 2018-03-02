62005918afe30b70fd77696acb80e7ff7c56fae6
lucianobispom <lucianobispom@gmail.com>
2018-03-01 21:57:08 -0300

    Classe UtilsExtensions criada

---
+++ ProgramadoraGet/Utils/UtilsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Utils
+{
+    public static class UtilsExtensions
+    {
+        public static string ToBase64(this byte[] buffer) => Convert.ToBase64String(buffer);
+
+        public static byte[] FromBase64(this string base64) => Convert.FromBase64String(base64);
+    }
+}
