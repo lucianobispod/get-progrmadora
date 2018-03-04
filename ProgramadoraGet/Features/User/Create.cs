9ee0587e8cf4f2278c958aa98c13cc459fea302d
Luciano Bispo <lucianobispom@gmail.com>
2018-03-04 00:37:50 -0300

    Criação de usuário feita

---
+++ ProgramadoraGet/Features/User/Create.cs
@@ -0,0 +1,89 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.User
+{
+    public class Create
+    {
+        public class Request : IRequest<Response>
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+
+            public string State { get; set; }
+
+            public string Location { get; set; }
+        }
+
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(validate => validate.Email).Length(20, 100).EmailAddress();
+                RuleFor(validate => validate.Password).Length(8, 20);
+                RuleFor(validate => validate.Description).MaximumLength(100);
+                RuleFor(validate => validate.Name).Length(8, 150).NotEmpty();
+            }
+        }
+
+        public class Response : Domain.User
+        {
+        }
+
+        public class Handler : AsyncRequestHandler<Request, Response>
+        {
+            private readonly Db db;
+
+            public Handler(Db db)
+            {
+                this.db = db;
+            }
+
+            protected override async Task<Response> HandleCore(Request request)
+            {
+                if (await db.Users.SingleOrDefaultAsync(s => s.Email == request.Email) != null) throw new Exception();
+
+                var user = new Domain.User
+                {
+                    Name = request.Name,
+                    Description = request.Description,
+                    Email = request.Email,
+                    State = request.State,
+                    Location = request.Location
+                };
+
+                user.SetPassword(request.Password);
+
+                db.Users.Add(user);
+
+                await db.SaveChangesAsync();
+
+                return new Response
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Description = user.Description,
+                    Email = user.Email,
+                    Location = user.Location,
+                    State = user.State,
+                    CreatedAt = user.CreatedAt,
+                    UpdatedAt = user.UpdatedAt
+                };
+            }
+        }
+
+
+    }
+}
---
+++ ProgramadoraGet/Features/User/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.User
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<Create.Response> Create([FromBody] Create.Request value)
+        {
+           return await mediator.Send(value);
+        }
+
+        
+    }
+}
