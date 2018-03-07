5427be069c0816ff90578f6c3f7087908153d1c4
Luciano Bispo <lucianobispom@gmail.com>
2018-03-06 21:12:40 -0300

    Classe de resposta pradrão criada

---
+++ ProgramadoraGet/Infrastructure/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ProgramadoraGet.Utils.ErrorMessagesHelper;
+
+namespace ProgramadoraGet.Infrastructure
+{
+    public class ApiResponse<T>
+    {
+        public T data;
+        public List<ErrorMessage> erros;
+    }
+}
