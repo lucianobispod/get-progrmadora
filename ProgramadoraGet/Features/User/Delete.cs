e9dafbd07ddc34d6b3d49a11820b59b939214f54
lucianobispom <lucianobispom@gmail.com>
2018-03-07 21:58:16 -0300

    classe delete implementada

---
+++ ProgramadoraGet/Features/User/Delete.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.User
+{
+    public class Delete
+    {
+        public class Model
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Id).NotEmpty().WithMessage("Identificador nulo");
+            }
+        }
+
+        public class Services
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async void Trash(Model model)
+            {
+                var user = await db.Users.FindAsync(model.Id);
+                
+                if (user == null) throw new Exception();
+
+                user.DeletedAt = DateTime.Now;
+
+                await db.SaveChangesAsync();
+            }
+
+        }
+    }
+}
