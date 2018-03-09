1026128a9fb1e5c41846a35da2a0677fa96ac8a5
Luciano Bispo <lucianobispom@gmail.com>
2018-03-09 15:07:10 -0300

    Criar tecnologia implementado

---
+++ ProgramadoraGet/Features/Technology/Create.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Technology
+{
+    public class Create
+    {
+        public class Model
+        {
+            public string Name { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Name)
+                   .NotEmpty().WithMessage("Nome não pode ser vazio")
+                   .MaximumLength(100).WithMessage("Limite de caracteres ultrapassado");
+            }
+        }
+
+
+        public class Services
+        {
+
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Tag> Save(Model model)
+            {
+                var technology = db.Tags.SingleOrDefault(s => s.Name.ToLower() == model.Name.ToLower() && s.TagType == Domain.TagType.Technology);
+
+                if (technology != null)
+                {
+                    if (technology.DeletedAt != null)
+                    {
+                        technology.DeletedAt = null;
+                        await db.SaveChangesAsync();
+                    }
+
+                    return technology;
+                }
+
+                technology = new Domain.Tag
+                {
+                    Name = model.Name,
+                    TagType = Domain.TagType.Technology
+                };
+
+                db.Tags.Add(technology);
+
+                await db.SaveChangesAsync();
+
+                return technology;
+
+            }
+        }
+    }
+}
---
+++ ProgramadoraGet/Features/Technology/TechnologyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.Technology
+{
+    [Route("api/[controller]")]
+    public class TechnologyController : Controller
+    {
+
+        private Db db;
+
+        public TechnologyController(Db db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<Domain.Tag> Create([FromBody] Create.Model model)
+        {
+            return await new Create.Services(db).Save(model);
+        }
+
+
+    }
+}
