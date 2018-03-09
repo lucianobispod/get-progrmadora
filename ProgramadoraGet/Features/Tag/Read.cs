4704fcf22382c189b1f378f43e4b3329bf79a9b2
elise_codexp <elise_codexp@outlook.com>
2018-03-09 20:11:54 -0300

    Adicionado Read (por Id, por TagType) no TagController + Correção do Route em Enterprise

---
+++ ProgramadoraGet/Features/Tag/Read.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Tag
+{
+    public class Read
+    {
+        //TODO: by ID
+        //TODO: by TagType
+        public class Model
+        {
+            public Guid Id { get; set; }
+            public Domain.TagType TagType { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model> {
+            public Validator()
+            {
+                //RuleFor(r => r.Id).NotEmpty().WithMessage("Identificador nulo");
+            }
+        }
+
+        public class Services
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<IList<Domain.Tag>> One(Model model)
+            {
+                if (model.Id == null) throw new Exception();
+
+                return await db.Tags
+                     .Where(t => t.DeletedAt == null)
+                     .Where(t => t.Id == model.Id)
+                     .Select(tag => new Domain.Tag
+                     {
+                         Id = tag.Id,
+                         Name = tag.Name,
+                         CreatedAt = tag.CreatedAt
+
+                     }).ToListAsync();
+            }
+
+            public async Task<IList<Domain.Tag>> ByTagType(Model model)
+            {
+
+                if (!Enum.IsDefined(typeof(Domain.TagType), model.TagType)) throw new Exception();
+
+                return await db.Tags
+                     .Where(t => t.DeletedAt == null)
+                     .Where(t => t.TagType == model.TagType)
+                     .Select(tag => new Domain.Tag
+                     {
+                         Id = tag.Id,
+                         Name = tag.Name,
+                         CreatedAt = tag.CreatedAt
+
+                     }).ToListAsync();
+            }
+
+
+
+        }
+
+
+
+    }
+}
--- ProgramadoraGet/Features/Tag/TagController.cs
+++ ProgramadoraGet/Features/Tag/TagController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProgramadoraGet.Infrastructure;
+using ProgramadoraGet.Utils;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,6 +26,52 @@ namespace ProgramadoraGet.Features.Tag
             return await new Create.Services(db).Save(model); 
         }
 
-      
+        [HttpGet]
+        [Route("{Id}")]
+        public async Task<DefaultResponse<IList<Domain.Tag>>> Read(Read.Model model)
+        {
+            var response = new DefaultResponse<IList<Domain.Tag>>();
+
+            if (!ModelState.IsValid)
+            {
+                response.erros = ErrorMessagesHelper.GetErrors(ModelState);
+                return response;
+            }
+
+            response.data = await new Read.Services(db).One(model);
+
+            return response;
+
+        }
+
+        [HttpGet]
+        [Route("byTagType/{TagType}")]
+        public async Task<DefaultResponse<IList<Domain.Tag>>> ReadByTagType(Read.Model model)
+        {
+            var response = new DefaultResponse<IList<Domain.Tag>>();
+
+            if (!ModelState.IsValid)
+            {
+                response.erros = ErrorMessagesHelper.GetErrors(ModelState);
+                return response;
+            }
+
+            response.data = await new Read.Services(db).ByTagType(model);
+
+            return response;
+
+        }
+
+        //[HttpGet]
+        //[Route("byTagType/normal")]
+        //public async Task<DefaultResponse<IList<Domain.Tag>>> ReadByTagTypeNormal(Read.Model model)
+        //{
+
+        //    return ReadByTagType(new Read.Model { TagType = 0 });
+
+
+        //}
+
+
     }
 }
