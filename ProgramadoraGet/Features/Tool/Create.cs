ecdfc6f7f7bca1bc4d5b58bb722990998bb4f62c
Luciano Bispo <lucianobispom@gmail.com>
2018-03-09 15:07:24 -0300

    criar ferramenta implementado

---
+++ ProgramadoraGet/Features/Tool/Create.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Tool
+{
+    public class Create
+    {
+        public class Model
+        {
+            public string Name { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Name)
+                   .NotEmpty().WithMessage("Nome não pode ser vazio")
+                   .MaximumLength(100).WithMessage("Limite de caracteres ultrapassado");
+            }
+        }
+
+
+        public class Services
+        {
+
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Tag> Save(Model model)
+            {
+                var tool = db.Tags.SingleOrDefault(s => s.Name.ToLower() == model.Name.ToLower()&& s.TagType == Domain.TagType.Tool);
+
+                if (tool != null)
+                {
+                    if (tool.DeletedAt != null)
+                    {
+                        tool.DeletedAt = null;
+                        await db.SaveChangesAsync();
+                    }
+
+                    return tool;
+                }
+
+                tool = new Domain.Tag
+                {
+                    Name = model.Name,
+                    TagType = Domain.TagType.Tool
+                };
+
+                db.Tags.Add(tool);
+
+                await db.SaveChangesAsync();
+
+                return tool;
+
+            }
+        }
+    }
+}
---
+++ ProgramadoraGet/Features/Tool/ToolController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.Tool
+{
+    [Route("api/[controller]")]
+    public class ToolController : Controller
+    {
+
+        private Db db;
+
+        public ToolController(Db db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<Domain.Tag> Create([FromBody] Create.Model model)
+        {
+            return await new Create.Services(db).Save(model);
+        }
+    }
+}
