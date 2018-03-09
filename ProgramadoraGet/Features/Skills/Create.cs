960c9b724633e5b8eac0741a384259eb5ab06b47
Luciano Bispo <lucianobispom@gmail.com>
2018-03-09 15:06:01 -0300

    criar e deletar Skills implementada

---
+++ ProgramadoraGet/Features/Skills/Create.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Skills
+{
+    public class Create
+    {
+        public class Model
+        {
+            public Guid UserId { get; set; }
+
+            public Guid TagId { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.TagId)
+                   .NotEmpty().WithMessage("Identificador de tag vazio");
+
+                RuleFor(r => r.UserId)
+                    .NotEmpty().WithMessage("Identificador de usuário vazio");
+            }
+        }
+
+        public class Services
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Skills> Save(Model model)
+            {
+                var user = await db.Users.FindAsync(model.UserId);
+
+                if (user == null) throw new Exception();
+                if (user.DeletedAt != null) throw new Exception();
+
+                var tag = await db.Tags.FindAsync(model.TagId);
+
+                if (tag == null) throw new Exception();
+                if (tag.DeletedAt != null) throw new Exception();
+                if (tag.TagType == Domain.TagType.Normal) throw new Exception();
+
+                var skills = new Domain.Skills
+                {
+                    UserId = user.Id,
+                    TagId = tag.Id
+                };
+
+                db.Skills.Add(skills);
+
+                await db.SaveChangesAsync();
+
+                return skills;
+
+            }
+        }
+
+    }
+}
---
+++ ProgramadoraGet/Features/Skills/SkillsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.Skills
+{
+    [Route("api/[controller]")]
+    public class SkillsController : Controller
+    {
+        private Db db;
+
+        public SkillsController(Db db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<Domain.Skills> Create([FromBody] Create.Model model)
+        {
+            return await new Create.Services(db).Save(model);
+        }
+
+
+        [HttpDelete]
+        public async Task Delete([FromBody] Delete.Model model)
+        {
+            await new Delete.Services(db).Trash(model);
+        }
+
+
+    }
+}
