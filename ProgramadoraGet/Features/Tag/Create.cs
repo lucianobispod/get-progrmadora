956fea0525351e890eb95e89cfdc3a37140f2f01
Luciano Bispo <lucianobispom@gmail.com>
2018-03-09 15:06:52 -0300

    Criar tag implementado

---
+++ ProgramadoraGet/Features/Tag/Create.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Tag
+{
+    public class Create
+    {
+        public class Model
+        {
+            public string Name { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Name)
+                   .NotEmpty().WithMessage("Nome não pode ser vazio")
+                   .MaximumLength(100).WithMessage("Limite de caracteres ultrapassado");
+            }
+        }
+
+
+        public class Services
+        {
+
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Tag> Save(Model model)
+            {
+                var tag = db.Tags.SingleOrDefault(s => s.Name.ToLower() == model.Name.ToLower() && s.TagType == Domain.TagType.Normal);
+
+                if (tag != null)
+                {
+                    if (tag.DeletedAt != null)
+                    {
+                        tag.DeletedAt = null;
+                        await db.SaveChangesAsync();
+                    }
+
+                    return tag;
+                }
+
+                tag = new Domain.Tag
+                {
+                    Name = model.Name,
+                    TagType = Domain.TagType.Normal
+                };
+
+                db.Tags.Add(tag);
+
+                await db.SaveChangesAsync();
+
+                return tag;
+
+            }
+        }
+    }
+}
---
+++ ProgramadoraGet/Features/Tag/TagController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.Tag
+{
+    [Route("api/[controller]")]
+    public class TagController : Controller
+    {
+        private Db db;
+
+        public TagController(Db db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<Domain.Tag> Create([FromBody] Create.Model model)
+        {
+            return await new Create.Services(db).Save(model); 
+        }
+
+      
+    }
+}
