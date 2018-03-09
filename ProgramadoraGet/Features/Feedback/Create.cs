5f7b0fe5aec8d58dbbc838900b1f780cea7dc19e
lucianobispom <lucianobispom@gmail.com>
2018-03-08 22:02:02 -0300

    Feedback implementado

---
+++ ProgramadoraGet/Features/Feedback/Create.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Feedback
+{
+    public class Create
+    {
+        public class Model 
+        {
+            public Guid UserId { get; set; }
+
+            public string Title { get; set; }
+
+            public string Content { get; set; }
+        }
+
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(validate => validate.Title)
+                    .NotEmpty().WithMessage("Titulo não pode ser vazio")
+                    .MaximumLength(100).WithMessage("Ultrapassado o limite de caracteres");
+
+                RuleFor(validate => validate.Content)
+                    .MaximumLength(200).WithMessage("Ultrapassado o limite de caracteres")
+                    .NotEmpty().WithMessage("Conteúdo não pode ser vazio");
+            }
+        }
+        
+        public class Services 
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Feedback> Save(Model model)
+            {
+                var feedback = new Domain.Feedback()
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    UserId = model.UserId
+                };
+
+                db.Feedbacks.Add(feedback);
+
+                await db.SaveChangesAsync();
+
+                return feedback;
+
+            }
+        }
+    }
+}
---
+++ ProgramadoraGet/Features/Feedback/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProgramadoraGet.Features.Feedback
+{
+    [Route("api/[controller]")]
+    public class FeedbackController : Controller
+    {
+        private readonly Db db;
+
+        public FeedbackController(Db db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<Domain.Feedback> Create([FromBody] Create.Model model)
+        {
+            return await new Create.Services(db).Save(model));
+        }
+
+        //[HttpGet]
+        //public async Task<IList<>> List()
+        //{
+        //    return await mediator.Send(new List.Query());
+
+        //}
+
+    }
+}
