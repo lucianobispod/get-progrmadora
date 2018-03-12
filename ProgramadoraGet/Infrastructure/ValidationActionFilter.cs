07850b7b01f9ce1f007b1737b1ffb5ee2508065b
Luciano Bispo <lucianobispom@gmail.com>
2018-03-12 14:45:47 -0300

    class validationactionfilter criada

---
+++ ProgramadoraGet/Infrastructure/ValidationActionFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using ProgramadoraGet.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Infrastructure
+{
+    public class ValidationActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var result = new ContentResult();
+
+                //string content = ErrorMessagesHelper.GetErrors(context.ModelState);
+
+                 string content = JsonConvert.SerializeObject(context.ModelState,
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                    });
+
+                result.Content = content;
+
+                result.ContentType = "application/json";
+
+                context.HttpContext.Response.StatusCode = 400;
+
+                context.Result = result;
+            }
+        }
+    }
+}
