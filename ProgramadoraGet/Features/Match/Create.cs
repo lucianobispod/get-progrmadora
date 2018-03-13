21a2485fddf13c63447a24a1438e10e617970d76
elise_codexp <elise_codexp@outlook.com>
2018-03-12 21:46:58 -0300

    Adicionado Controller e Create de Match

---
+++ ProgramadoraGet/Features/Match/Create.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.Match
+{
+    public class Create
+    {
+        public class Model
+        {
+            public Guid UserId { get; set; }
+            public Guid EnterpriseId { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.UserId)
+                   .NotEmpty().WithMessage("Identificador de usuário vazio");
+                RuleFor(r => r.EnterpriseId)
+                   .NotEmpty().WithMessage("Identificador de empresa vazio");
+            }
+        }
+
+        public class Services
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.Match> Save(Model model)
+            {
+                var user = await db.Users.FindAsync(model.UserId);
+
+                if (user == null) throw new Exception();
+                if (user.DeletedAt != null) throw new Exception();
+
+                var enterprise = await db.Tags.FindAsync(model.EnterpriseId);
+
+                if (enterprise == null) throw new Exception();
+                if (enterprise.DeletedAt != null) throw new Exception();
+
+                var match = new Domain.Match
+                {
+                    UserId = user.Id,
+                    EnterpriseId = enterprise.Id
+                };
+                
+                db.Matchs.Add(match);
+
+                await db.SaveChangesAsync();
+
+                return match;
+            }
+        }
+
+
+
+    }
+}
---
+++ ProgramadoraGet/Features/Match/MatchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgramadoraGet.Infrastructure;
+
+namespace ProgramadoraGet.Features.Match
+{
+    [Route("api/[controller]")]
+    public class MatchController : Controller
+    {
+        //TODO: ReadOne
+        //TODO: ReadAll
+        //TODO: Create
+        //TODO: Update
+        //TODO: Delete
+
+        private Db db;
+
+        public MatchController(Db db)
+        {
+            this.db = db;
+        }
+
+        //[HttpPost]
+
+
+    }
+}
