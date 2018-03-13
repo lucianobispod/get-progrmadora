c450632c7d2b7adbf1427c3d37be7beb2b4d1565
Luciano Bispo <lucianobispom@gmail.com>
2018-03-13 14:51:18 -0300

    classe Me na features usuário criada. Pega as informacões do usuario logado

---
+++ ProgramadoraGet/Features/User/Me.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using ProgramadoraGet.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramadoraGet.Features.User
+{
+    public class Me
+    {
+        public class Model
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Model>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Id).NotEmpty().WithMessage("Token inválido");
+            }
+        }
+
+        public class Services
+        {
+            private readonly Db db;
+
+            public Services(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Domain.User> Me(Model model)
+            {
+                var user = await db.Users.FindAsync(model.Id);
+
+                if (user == null) throw new Exception();
+
+                if (user != null && user.DeletedAt != null) throw new Exception();
+
+                return user;
+            }
+        }
+    }
+}
