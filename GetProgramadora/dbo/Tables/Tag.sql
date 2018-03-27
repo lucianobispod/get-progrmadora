26effd7f337082102626e62952e7cec759894ce2
Luciano Bispo <lucianobispom@gmail.com>
2018-03-27 16:04:41 -0300

    atualização do banco de dados

--- GetProgramadora/dbo/Tables/Tag.sql
+++ GetProgramadora/dbo/Tables/Tag.sql
@@ -3,8 +3,11 @@
     [CreatedAt] DATETIME2 (7)    DEFAULT (getdate()) NOT NULL,
     [DeletedAt] DATETIME2 (7)    NULL,
     [Name]      NVARCHAR (100)   NOT NULL,
+    [TagType]   INT              NOT NULL,
     CONSTRAINT [PK_Tag] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
