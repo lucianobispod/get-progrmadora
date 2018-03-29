e4dfe84dd1a254cdcba26c952085cb8e72eb7e71
Luciano Bispo <lucianobispom@gmail.com>
2018-03-29 01:13:00 -0300

    metódos encrypt e decrypt adicionados

--- ProgramadoraGet/Domain/RecoveryPassword.cs
+++ ProgramadoraGet/Domain/RecoveryPassword.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProgramadoraGet.Domain
@@ -19,7 +22,81 @@ namespace ProgramadoraGet.Domain
 
         public virtual User User { get; set; }
 
-
         #endregion
+
+
+        private byte[] key = Encoding.ASCII.GetBytes("sqpdlrai");
+        private byte[] IV = Encoding.ASCII.GetBytes("senai132");
+
+        private byte[] encrypted;
+
+
+        public string Encrypt(string plainText)
+        {
+            try
+            {
+                using (DESCryptoServiceProvider crypto = new DESCryptoServiceProvider())
+                {
+                    crypto.Key = this.key;
+                    crypto.IV = this.IV;
+
+                    ICryptoTransform transform = crypto.CreateEncryptor(crypto.Key, crypto.IV);
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(stream, transform, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter writer = new StreamWriter(cryptoStream))
+                            {
+                                writer.Write(plainText);
+                            }
+
+                            encrypted = stream.ToArray();
+                        }
+                    }
+                }
+
+                return Convert.ToBase64String(encrypted);
+            }
+            catch (Exception ex)
+            {
+
+                { throw ex; }
+            }
+        }
+
+        public Guid Decrypt(string cipherText)
+        {
+            try
+            {
+                string plainText = string.Empty;
+
+                using (DESCryptoServiceProvider crypto = new DESCryptoServiceProvider())
+                {
+                    crypto.Key = this.key;
+                    crypto.IV = this.IV;
+
+                    ICryptoTransform transform = crypto.CreateDecryptor(crypto.Key, crypto.IV);
+
+                    using (MemoryStream stream = new MemoryStream(Convert.FromBase64String(cipherText)))
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(stream, transform, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader reader = new StreamReader(cryptoStream))
+                            {
+                                plainText = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+
+                return Guid.Parse(plainText);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
